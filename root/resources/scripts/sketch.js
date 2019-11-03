b939975f54b223513eabd4561bcfd9e3457c532e
William <william.hemmingsson@gmail.com>
2019-11-03 22:11:24 +0100

    Added some more tiles

--- root/resources/scripts/sketch.js
+++ root/resources/scripts/sketch.js
@@ -1,11 +1,43 @@
+let tileImages = [];
+
 function setup() {
     let canvas = createCanvas(850,700);
     canvas.parent('sketch-holder');
     
     console.log(tiles);
+
+    loadTilesImages();
+}
+function draw() {
+    renderTiles();    
 }
 
-function draw() {
+function loadTilesImages(){
+    tiles.forEach(tile => {
+        let path = "resources/images/" + tile.id + ".png";
+        let img = loadImage(path, 
+            () => { 
+                console.log("Finished loading " + path ); 
+                tileImages.push(img);
+            },
+            () => console.log("Error loading " + path ));        
+    });
+}
+
+function renderTiles() {
+    let currentX = 0;
+    let currentY = 0;
+    let counter = 0;
+    tileImages.forEach(img => {
+        image(img, currentX, currentY, img.width / 4, img.height / 4);
+        currentX += img.width / 4 + 5;
+        counter++;
+
+        if(counter % 8 == 0){
+            currentX = 0;
+            currentY += img.height / 4 + 5;
+        }
+    });
 }
   
 
