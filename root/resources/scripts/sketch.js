7928ff7133078ad34ae5a49e87198793f493edc6
William <william.hemmingsson@gmail.com>
2019-11-03 20:42:40 +0100

    Added p5.js

---
+++ root/resources/scripts/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    let canvas = createCanvas(850,700);
+	canvas.parent('sketch-holder');
+}
+
+function draw() {
+}
+  
+
+function mouseClicked() { 
+}
+
+function mouseMoved() {
+}
+
+function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min) + min); 
+}
